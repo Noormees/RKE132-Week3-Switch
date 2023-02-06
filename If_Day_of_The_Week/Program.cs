c144811ade6fff07b17551ebc0abc0a66fb85966
Noormees <garry.lobanoff@gmail.com>
2023-02-06 20:17:40 +0200

    add week3 code

---
+++ If_Day_of_The_Week/Program.cs
@@ -0,0 +1,39 @@
+﻿// tuvastame, mis nadala paev tana on (01.01.1970)
+
+
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+if (weekDay == 0)
+{
+    Console.WriteLine("It's Sunday!");
+}
+else if (weekDay == 1)
+{
+    Console.WriteLine("It's Monday :(");
+
+}
+else if (weekDay == 2)
+{
+    Console.WriteLine("It's Tuesday");
+}
+else if (weekDay == 3)
+{
+    Console.WriteLine("It's Wednesday");
+}
+else if (weekDay == 4)
+{
+    Console.WriteLine("It's Thuesday");
+}
+else if (weekDay == 5)
+{
+    Console.WriteLine("It's Friday!");
+
+}
+else {
+    Console.WriteLine("It's Satyrday!");
+}
+
+Console.WriteLine("Have a nice Day!");
+
+
+
---
+++ Odd_or_Even/Program.cs
@@ -0,0 +1,20 @@
+﻿// küsime kasutajal sisestada numbri
+
+Console.WriteLine("Please enter a number:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+// kontrollime, kas number on paaris voi paaritu
+
+int result = userNumber % 2;
+
+Console.WriteLine(result);
+
+if (result != 0)    // != EI VORDU
+
+{
+    Console.WriteLine("Your number is Odd.");
+}
+else
+{
+    Console.WriteLine("Your number is Even."); 
+}
